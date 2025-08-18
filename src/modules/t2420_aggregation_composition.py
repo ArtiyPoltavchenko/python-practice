b39fc3d47fad15e14a000cdfa0322529994cd514
arsentii.poltavchenko@powercoders.org <arsentii.poltavchenko@powercoders.org>
2025-08-18 13:46:27 +0200

    FIX: Classes dependencies now more OOP Pythonic

--- src/modules/t2420_aggregation_composition.py
+++ src/modules/t2420_aggregation_composition.py
@@ -6,53 +6,78 @@ class BookContainer:
     def __init__(self):
         self.booksByAuthor = {} # Aggregation - Books are independent and can leave without BookContainer, but can be found in it. 
 
+    def ensureList(self, books):
+        if isinstance(books, BorrowedBook):
+            books = [books]
+        return books
 
     def addBooks(self, books): # now with Bulk method support!
-        if isinstance(books, Book):
-            books = [books] # in case if it will be single element
-        for book in books:
-            CheckIfValid.dataType(book, Book)
-            key = book.author if book.author else "Unknown"
+        
+        for book in self.ensureList(books):
+            CheckIfValid.dataType(book, BorrowedBook)
+            key = book.author.name #could be "Unknown"
             self.booksByAuthor.setdefault(key, []).append(book)
     
     def removeBooks(self, books):
-        if isinstance(books, Book):
-            books = [books]
-        for book in books:
-            CheckIfValid.dataType(book, Book)
-            if book.author in self.booksByAuthor:
+        for book in self.ensureList(books):
+            CheckIfValid.dataType(book, BorrowedBook)
+            if book.author.name in self.booksByAuthor:
                 try:
-                    self.booksByAuthor[book.author].remove(book)
+                    self.booksByAuthor[book.author.name].remove(book)
                 except ValueError:
                         pass
-                if not self.booksByAuthor[book.author]:  #delete if no books of this author
-                    del self.booksByAuthor[book.author]
+                if not self.booksByAuthor[book.author.name]:  #delete if no books of this author
+                    del self.booksByAuthor[book.author.name]
+
+    def updateBooks(self, books):
+        for book in self.ensureList(books):
+            CheckIfValid.dataType(book, BorrowedBook)
+            for authorName, booksList in list(self.booksByAuthor.items()):
+                if book in booksList:
+                    booksList.remove(book)
+                    if not booksList:
+                        del self.booksByAuthor[authorName]
+                    break
+            self.booksByAuthor.setdefault(book.author.name, []).append(book)
                         
 
 class Library(BookContainer):
-    def __init__(self, name, address):
+    def __init__(self, name, city, street, building):
         super().__init__()
-        self.name = name        #]
+        self.name = name        
                                   # } Composition. Library gone - atributs gone.
-        self.address = address  #]   
+        self.address = Address(city, street, building)    
+
+
     # easy example - no @properties 
 
+class Address:
+    def __init__(self, city, street, building):
+        self.city = city
+        self.street = street
+        self.building = building 
 
-
-class Author(BookContainer):
+class Author:
     def __init__(self, name):
-        super().__init__()
         self.name = name
 
 class Book:
     def __init__(self, title, description, author=None):
         self.title = title
         self.description = description
-        self.author = author
+        self._author = author if author else Author("Unknown")
     
+    
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        self._author = new_author
     
 
-class LibraryBook(Book):
+class BorrowedBook(Book):
     def __init__(self, title, description, author=None):
         super().__init__(title, description, author)
         self.library = None
@@ -71,20 +96,34 @@ class LibraryBook(Book):
 
 
 #Create Library
-libray = Library("BookWorm", "SomewhereStr. 42")
+libray = Library("BookWorm", "HappyTown", "SomewhereStreet", "42B")
 
-#Fill with books and authors
+# Fill with books and authors
 for i in range(3):
     author = Author(f"Author {i}")
     books = []
     for j in range(5):
-        books.append(LibraryBook(f"Book {i}+{j}", f"Description {i}+{j}", author))
+        newBook = BorrowedBook(f"Book {i}+{j}", f"Description {i}+{j}", author)
+        newBook.library = libray
+        books.append(newBook)
     libray.addBooks(books)
-libray.addBooks(LibraryBook("Lonely Book", "Just to test single adding", author=None)) #with no Author
 
-for author in libray.booksByAuthor.keys():
-    for book in libray.booksByAuthor[author]:
-        print(f"Title: {book.title}, Desc: {book.description}, Author: {book.author.name if book.author else book.author}")
+libray.addBooks(BorrowedBook("Lonely Book", "Just to test single adding", author=None)) #with no Author
+
+print(libray.booksByAuthor.keys())
+
+bookToUpdate = libray.booksByAuthor["Unknown"][0]
+bookToUpdate.author = Author("Nick")
+libray.updateBooks(bookToUpdate)
+
+#print(libray.booksByAuthor["Unknown"][0].author.name)
+print(libray.booksByAuthor.keys())
+
+for authorName in libray.booksByAuthor.keys():
+    for book in libray.booksByAuthor[authorName]:
+        print(f"Key:{authorName} | Title: {book.title}, Desc: {book.description}, Author: {book.author.name}")
+
+
 
 
 
